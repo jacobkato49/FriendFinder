2342572301b861d6472f9a459f80e451bf29df46
Jacob Kato <jacobkato49@yahoo.com>
2017-08-06 18:42:24 -0700

    debugging

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -8,6 +8,6 @@ module.exports = function(app){
   })
 
   app.use(function(req,res){
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
 }
